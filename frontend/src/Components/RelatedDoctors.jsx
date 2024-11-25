fb54a892d70bd66d4e9a9d6d35b993b593ff600b
Jose <aliemhejosemaria@gmail.com>
2024-11-25 10:32:03 +0100

    on profilr page frontend

--- frontend/src/Components/RelatedDoctors.jsx
+++ frontend/src/Components/RelatedDoctors.jsx
@@ -1,17 +1,27 @@
-import React, { useContext } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../Context/AppContext'
+import { useNavigate } from 'react-router-dom'
 
-  const TopDoctors = () => {
+const RelatedDoctors = ({docId,speciality}) => {
 
-  const navigate = useNavigate()
-  const {doctors} = useContext(AppContext)
+    const {doctors} = useContext(AppContext)
+    const navigate = useNavigate()
+
+    const [relDoc, setRelDoc] = useState([])
+
+    useEffect(()=>{
+    if(doctors.length > 0 && speciality){
+        const doctorsData = doctors.filter((doc)=> doc.speciality === speciality && doc._id !== docId )
+        setRelDoc(doctorsData)
+    } 
+
+    }, [doctors,speciality,docId])
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
         <h1 className='text-3xl font-medium'>Top Doctors to book</h1>
         <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted Doctors.</p>
         <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
-            {doctors.slice(0,12).map((item,index)=>(
+            {relDoc.slice(0,5).map((item,index)=>(
                  <div onClick={()=>{navigate(`/appointment/${item._id}`); scrollTo(0,0)}} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
                     <img className='bg-blue-50' src={item.image} alt="" />
                     <div className='p-4'>
@@ -29,4 +39,4 @@ import { AppContext } from '../Context/AppContext'
   )
 }
 
-export default TopDoctors
+export default RelatedDoctors
